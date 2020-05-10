27db18bf3cc94c6542870053bf57b3318f01efae
Mahipal Jat <mahipaljyani@gmail.com>
2020-05-10 21:52:20 +0530

    gai sab ki sab

--- public/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-app.use(express.json())    // <==== parse request body as JSON
-
-app.listen(8080)
-
-app.post('/test', (req, res) => {
-  res.json({requestBody: req.body})  // <==== req.body will be a parsed JSON object
-})
